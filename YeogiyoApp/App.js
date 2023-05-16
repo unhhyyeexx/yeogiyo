cf17ab97d652a8b672c7543d16ed5d3af8429899
unhhyyeexx <unhhyyee66@gmail.com>
2023-05-16 20:53:26 +0900

    feat: 🎸 create yeogiyoApp & navigation Setting
    create yeogiyoApp & navigation Setting

---
+++ YeogiyoApp/App.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Home from "./screens/Home";
+import Display from "./screens/Display";
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Display" component={Display} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ YeogiyoApp/screens/Display.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function Display() {
+  return (
+    <View>
+      <Text>Display!</Text>
+    </View>
+  );
+}
+
+export default Display;
