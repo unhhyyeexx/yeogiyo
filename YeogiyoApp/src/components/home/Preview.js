48acf7ec0ec5873d33b5d679df6d045862c5840e
unhhyyeexx <unhhyyee66@gmail.com>
2023-05-18 20:27:32 +0900

    feat: 🎸 apply text scolling function

--- YeogiyoApp/src/components/home/Preview.js
+++ YeogiyoApp/src/components/home/Preview.js
@@ -1,23 +1,48 @@
-import React from "react";
-import { StyleSheet, View, Text, Dimensions } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View, Text, Dimensions, Pressable } from "react-native";
+import TextTicker from "react-native-text-ticker";
 
 const valueHeight = Dimensions.get("window").height;
 const valueWidth = Dimensions.get("window").width;
 
 function Preview({ navigation }) {
+  const times = [2000, 1500, 1000, 500, 100];
+  const [slideTime, setST] = useState(2000);
+  const tabScreen = () => {
+    setST((slideTime + 1) % 5);
+    console.log(times[slideTime]);
+  };
   return (
-    <View style={styles.pvContainer}>
-      <Text>preview!</Text>
-    </View>
+    <Pressable style={styles.pvContainer} onPress={tabScreen}>
+      <TextTicker
+        duration={times[slideTime]}
+        bounce={false}
+        bounceSpeed={0}
+        bounceDelay={0}
+        repeatSpacer={0}
+        marqueeOnMount={true}
+        marqueeDelay={0}
+        disabled={true}
+        style={styles.pvText}
+      >
+        프리뷰!
+      </TextTicker>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   pvContainer: {
     margin: 0,
+    alignItems: "center",
     width: "100%",
     height: valueWidth ** 2 / valueHeight,
     backgroundColor: "grey",
   },
+  pvText: {
+    margin: 0,
+    fontSize: valueWidth ** 2 / valueHeight - 30,
+  },
 });
+
 export default Preview;
